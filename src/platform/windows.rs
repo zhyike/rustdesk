9107d9b2b56ed4645a6bdc168158962dd7d3b150
zhyike <44109121+zhyike@users.noreply.github.com>
2026-09-23 10:27:40 +0800

    Update windows.rs

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1924,8 +1924,9 @@ pub fn add_recent_document(path: &str) {
 }
 
 pub fn is_installed() -> bool {
-    let (_, _, _, exe) = get_install_info();
-    std::fs::metadata(exe).is_ok()
+    // let (_, _, _, exe) = get_install_info();
+    // std::fs::metadata(exe).is_ok()
+    true
 }
 
 pub fn get_reg(name: &str) -> String {
